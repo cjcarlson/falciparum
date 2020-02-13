e460f098a1f4b8ead2e34c2037a8420f942f7499
Shopnavo Biswas <Shop4navo@Shopnavos-MacBook-Air.local>
2020-02-13 02:29:53 -0600

    prelim correlation figures

--- 03 - Shop test.R
+++ 03 - Shop test.R
@@ -17,6 +17,10 @@ library(lubridate)
 library(readr)
 library(sp)
 library(rgeos)
+library(plyr)
+library(broom)
+library(data.table)
+library(lme4)
 
 user = "Shop" #"Colin"
 if (user == "Colin") {
@@ -58,6 +62,32 @@ first <- data.frame(R0 = cont$Jan.1900.R0,
                     temp2 = cont$Jan.1900.temp2,
                     ppt = cont$Jan.1900.ppt,
                     ppt2 = cont$Jan.1900.ppt2)
+slope2 <- function(x, y){
+  return(cov(x, y)/var(x))
+}
+
+drops = c("Pf","PfPR2")
+edited = data[,!(names(data) %in% drops)]
+edited2 = edited[complete.cases(edited), ]
+grouped = edited2 %>% group_by(OBJECTID)
+lms = grouped %>% do(model = summary(lm(temp ~ year, data = .)))
+#below line to be taken out later
+models = lms$model
+
+models <- edited2 %>% group_by(OBJECTID) %>% summarise(mod = list(lm(temp ~ year)))
+
+dat <- data.table(x=edited2$year, y=edited2$temp, grp=edited2$OBJECTID)
+model = dat[,coef(lm(temp~year)),by=grp]
+
+model = lmList(temp ~ year | OBJECTID, data=edited2)
+rows = rownames(coef(model))
+coefs = coef(model)$year
+df <- data.frame(matrix(ncol = 2, nrow = 847))
+x <- c("OBJECTID", "cors")
+colnames(df) <- x
+df$OBJECTID = rows
+df$cors = coefs
+df$OBJECTID <- factor(df$OBJECTID)
 
 meancovs = c('temp','ppt')
 avgdf = data %>% group_by(OBJECTID) %>% summarize_at(meancovs, mean, na.rm = T)
@@ -69,22 +99,24 @@ completeFun <- function(data, desiredCols) {
 }
 
 #cont <- cont[cont@data$OBJECTID %in% avgdf$OBJECTID,]
-cont@data <- left_join(cont@data, avgdf, by = c("OBJECTID" = "OBJECTID"))
+##changed df from avgdf to df
+cont@data <- left_join(cont@data, df, by = c("OBJECTID" = "OBJECTID"))
+#cont@data <- left_join(cont@data, avgdf, by = c("OBJECTID" = "OBJECTID"))
 #cont@data <- completeFun(cont@data,'ppt')
 
 cont@data$id = rownames(cont@data)
 cont.points = fortify(cont, region ="id" )
 cont.df = merge(cont.points, cont@data, by="id")
 
-#to_plot = cont.df[cont.df$NAME_0 == 'Chad',]
+to_plot = cont.df[cont.df$NAME_0 == 'Nigeria',]
 #to_plot = cont.df[seq(1, nrow(cont.df), 5), ]
-to_plot = cont.df
+#to_plot = cont.df
 gg <- ggplot() + geom_polygon(data = to_plot, aes(x=long, y=lat, group = group,
                                                                       fill = ppt)) +
   guides(colour="none") + 
   #geom_line(linetype="dotted", color="black", size=5) +
   coord_fixed()+
-  scale_fill_gradient2(low = "yellow", mid="red", high = "blue", midpoint = median(to_plot[,'ppt'], na.rm = TRUE), guide = guide_colorbar(title="Precipitation")) +
+  scale_fill_gradient2(low = "yellow", mid="red", high = "blue", midpoint = median(to_plot[,'ppt'], na.rm = TRUE), guide = guide_colorbar(title="Correlations")) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
   labs(title= "Test graph",
@@ -93,7 +125,7 @@ gg <- ggplot() + geom_polygon(data = to_plot, aes(x=long, y=lat, group = group,
 gg
 
 gg <- ggplot() + geom_polygon(data = cont.df[seq(1, 4251569,5),], aes(x=long, y=lat, group = group,
-                                                                      fill = ppt)) +
+                                                                      fill = cors.x)) +
   coord_fixed()+
   scale_fill_gradient2(low = "yellow", mid="red", high = "blue", midpoint = 100, guide = guide_colorbar(title="Precipitation")) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
