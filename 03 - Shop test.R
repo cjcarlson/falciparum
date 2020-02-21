697d1686b34b5c7e537dfc1db50f4ba38b7c2dc7
Shopnavo Biswas <Shop4navo@Shopnavos-MacBook-Air.local>
2020-02-20 18:06:45 -0600

    almost done with generating prelim averages

--- 03 - Shop test.R
+++ 03 - Shop test.R
@@ -70,14 +70,14 @@ drops = c("Pf","PfPR2")
 edited = data[,!(names(data) %in% drops)]
 edited2 = edited[complete.cases(edited), ]
 grouped = edited2 %>% group_by(OBJECTID)
-lms = grouped %>% do(model = summary(lm(temp ~ year, data = .)))
+#lms = grouped %>% do(model = summary(lm(temp ~ year, data = .)))
 #below line to be taken out later
-models = lms$model
+#models = lms$model
 
-models <- edited2 %>% group_by(OBJECTID) %>% summarise(mod = list(lm(temp ~ year)))
+#models <- edited2 %>% group_by(OBJECTID) %>% summarise(mod = list(lm(temp ~ year)))
 
-dat <- data.table(x=edited2$year, y=edited2$temp, grp=edited2$OBJECTID)
-model = dat[,coef(lm(temp~year)),by=grp]
+#dat <- data.table(x=edited2$year, y=edited2$temp, grp=edited2$OBJECTID)
+#model = dat[,coef(lm(temp~year)),by=grp]
 
 model = lmList(temp ~ year | OBJECTID, data=edited2)
 rows = rownames(coef(model))
@@ -93,43 +93,39 @@ meancovs = c('temp','ppt')
 avgdf = data %>% group_by(OBJECTID) %>% summarize_at(meancovs, mean, na.rm = T)
 avgdf$OBJECTID <- factor(avgdf$OBJECTID)
 
-completeFun <- function(data, desiredCols) {
-  completeVec <- complete.cases(data[, desiredCols])
-  return(data[completeVec, ])
-}
+#completeFun <- function(data, desiredCols) {
+#  completeVec <- complete.cases(data[, desiredCols])
+#  return(data[completeVec, ])
+#}
 
 #cont <- cont[cont@data$OBJECTID %in% avgdf$OBJECTID,]
 ##changed df from avgdf to df
 cont@data <- left_join(cont@data, df, by = c("OBJECTID" = "OBJECTID"))
-#cont@data <- left_join(cont@data, avgdf, by = c("OBJECTID" = "OBJECTID"))
+cont@data <- left_join(cont@data, avgdf, by = c("OBJECTID" = "OBJECTID"))
 #cont@data <- completeFun(cont@data,'ppt')
 
 cont@data$id = rownames(cont@data)
 cont.points = fortify(cont, region ="id" )
 cont.df = merge(cont.points, cont@data, by="id")
 
+
 to_plot = cont.df[cont.df$NAME_0 == 'Nigeria',]
 #to_plot = cont.df[seq(1, nrow(cont.df), 5), ]
 #to_plot = cont.df
+
+plot_list <- list("temp","cors","ppt")
+
+for (value in plot_list) {
 gg <- ggplot() + geom_polygon(data = to_plot, aes(x=long, y=lat, group = group,
-                                                                      fill = ppt)) +
+                                                                      fill = value),color=NA) +
   guides(colour="none") + 
   #geom_line(linetype="dotted", color="black", size=5) +
   coord_fixed()+
-  scale_fill_gradient2(low = "yellow", mid="red", high = "blue", midpoint = median(to_plot[,'ppt'], na.rm = TRUE), guide = guide_colorbar(title="Correlations")) +
+  scale_fill_gradient2(low = "yellow", mid="red", high = "blue", midpoint = median(to_plot[,value], na.rm = TRUE), guide = guide_colorbar(title=value)) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
-  labs(title= "Test graph",
+  labs(title= paste(value,"graph",sep = " ", collapse = NULL),
        x="Longitude", y= "Latitude") 
 ## cut out formatting fluff for debugging
 gg
-
-gg <- ggplot() + geom_polygon(data = cont.df[seq(1, 4251569,5),], aes(x=long, y=lat, group = group,
-                                                                      fill = cors.x)) +
-  coord_fixed()+
-  scale_fill_gradient2(low = "yellow", mid="red", high = "blue", midpoint = 100, guide = guide_colorbar(title="Precipitation")) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
-  labs(title= "Test graph",
-       x="Longitude", y= "Latitude") 
-
+ggsave(paste(value,'.png',sep="",collapse=NULL))}
