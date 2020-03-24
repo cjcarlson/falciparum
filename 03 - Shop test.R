6eaa468f5f9a0f0ee1ddb3f754c4e9a5ba051ed7
Shopnavo Biswas <Shop4navo@shopnavos-air.lan>
2020-03-24 01:24:18 -0700

    final graph generating notebook and R script

--- 03 - Shop test.R
+++ 03 - Shop test.R
@@ -56,23 +56,12 @@ cont = SpatialPolygonsDataFrame(temp2, data=temp@data)
 #cont@data$OBJECTID = as.numeric(as.character(cont@data$OBJECTID))
 #cont@data$OBJECTID = as.numeric(cont@data$OBJECTID)
 
-month = c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec')
-
-first <- data.frame(R0 = cont$Jan.1900.R0,
-                    Pf = cont$Jan.1900.Pf,
-                    Pf2 = cont$Jan.1900.PfPR2,
-                    temp = cont$Jan.1900.temp,
-                    temp2 = cont$Jan.1900.temp2,
-                    ppt = cont$Jan.1900.ppt,
-                    ppt2 = cont$Jan.1900.ppt2)
-slope2 <- function(x, y){
-  return(cov(x, y)/var(x))
-}
 
 drops = c("Pf","PfPR2")
 edited = data[,!(names(data) %in% drops)]
 edited2 = edited[complete.cases(edited), ]
-grouped = edited2 %>% group_by(OBJECTID)
+pre_edited2 = edited2[edited2$year < 1980,]
+post_edited2 = edited2[edited2$year >= 1980,]
 #lms = grouped %>% do(model = summary(lm(temp ~ year, data = .)))
 #below line to be taken out later
 #models = lms$model
@@ -82,14 +71,29 @@ grouped = edited2 %>% group_by(OBJECTID)
 #dat <- data.table(x=edited2$year, y=edited2$temp, grp=edited2$OBJECTID)
 #model = dat[,coef(lm(temp~year)),by=grp]
 
-model = lmList(temp ~ year | OBJECTID, data=edited2)
-rows = rownames(coef(model))
-coefs = coef(model)$year
+full_temp_model = lmList(temp ~ year | OBJECTID, data=edited2)
+pre_temp_model = lmList(temp ~ year | OBJECTID, data=pre_edited2)
+post_temp_model = lmList(temp ~ year | OBJECTID, data=post_edited2)
+full_ppt_model = lmList(ppt ~ year | OBJECTID, data=edited2)
+pre_ppt_model = lmList(ppt ~ year | OBJECTID, data=pre_edited2)
+post_ppt_model = lmList(ppt ~ year | OBJECTID, data=post_edited2)
+full_temp_coefs = coef(full_temp_model)$year
+pre_temp_coefs = coef(pre_temp_model)$year
+post_temp_coefs = coef(post_temp_model)$year
+full_ppt_coefs = coef(full_ppt_model)$year
+pre_ppt_coefs = coef(pre_ppt_model)$year
+post_ppt_coefs = coef(post_ppt_model)$year
+rows = rownames(coef(full_temp_model))
 df <- data.frame(matrix(ncol = 2, nrow = 847))
-x <- c("OBJECTID", "cors")
+x <- c("OBJECTID", "full_temp_cors","pre_temp_cors","post_temp_cors","full_ppt_cors","pre_ppt_cors","post_ppt_cors")
 colnames(df) <- x
 df$OBJECTID = rows
-df$cors = coefs
+df$full_temp_cors = full_temp_coefs
+df$pre_temp_cors = pre_temp_coefs
+df$post_temp_cors = post_temp_coefs
+df$full_ppt_cors = full_ppt_coefs
+df$pre_ppt_cors = pre_ppt_coefs
+df$post_ppt_cors = post_ppt_coefs
 #df$OBJECTID = as.numeric(as.character(df$OBJECTID))
 #df$OBJECTID <- factor(df$OBJECTID)
 
@@ -111,9 +115,9 @@ cont@data$id = rownames(cont@data)
 cont.points = fortify(cont, region ="id" )
 cont.df = merge(cont.points, cont@data, by="id")
 
-to_plot = cont.df#[cont.df$NAME_0 == 'Nigeria',]
+#to_plot = cont.df[cont.df$NAME_0 == 'Nigeria',]
 #to_plot = cont.df[seq(1, nrow(cont.df), 5), ]
-#to_plot = cont.df
+to_plot = cont.df
 
 # plot_list <- list("temp","cors","ppt")
 # plot_list2 <- list("temp")
@@ -134,21 +138,91 @@ to_plot = cont.df#[cont.df$NAME_0 == 'Nigeria',]
 # #gg
 # ggsave(paste(value,'.png',sep="",collapse=NULL))}
 
-# CORRELATIONS
+# TEMP CORRELATIONS
 gg <- ggplot() + geom_polygon(data = to_plot, na.rm=TRUE, aes(x=long, y=lat, group = group,
-                                                              fill = as.numeric(to_plot[,'cors'])),color=NA) +
+                                                              fill = as.numeric(to_plot[,'full_temp_cors'])),color=NA) +
   guides(colour="none") + 
   #geom_line(linetype="dotted", color="black", size=5) +
   coord_fixed()+
-  scale_fill_gradient2(low = '#e6e6ff', mid="#0000ff", high = "#000080", midpoint = median(to_plot[,'cors'], na.rm = TRUE), guide = guide_colorbar(title='Correlation Coefficients')) +
+  scale_fill_gradient2(low = '#ffd9b3', mid="#ff8000", high = "#804000", midpoint = median(to_plot[,'full_temp_cors'], na.rm = TRUE), guide = guide_colorbar(title='Correlation Coefficients (ºC/year)')) +
   #scale_color_gradientn(colours=rev(brewer.pal(4,"RdYlBu")),guide = guide_colorbar(title='value')) +
   #scale_fill_brewer(palette="RdYlBu") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
-  labs(title= "Temperature Increase per year",x="Longitude", y= "Latitude") 
+  labs(title= "Temperature Increase per Year 1900 - 2017",x="Longitude", y= "Latitude") 
 ## cut out formatting fluff for debugging
-#gg
-ggsave('cors.png')
+ggsave('full_temp_cors.png')
+
+gg <- ggplot() + geom_polygon(data = to_plot, na.rm=TRUE, aes(x=long, y=lat, group = group,
+                                                              fill = as.numeric(to_plot[,'pre_temp_cors'])),color=NA) +
+  guides(colour="none") + 
+  #geom_line(linetype="dotted", color="black", size=5) +
+  coord_fixed()+
+  scale_fill_gradient2(low = '#ffd9b3', mid="#ff8000", high = "#804000", midpoint = median(to_plot[,'pre_temp_cors'], na.rm = TRUE), guide = guide_colorbar(title='Correlation Coefficients (ºC/year)')) +
+  #scale_color_gradientn(colours=rev(brewer.pal(4,"RdYlBu")),guide = guide_colorbar(title='value')) +
+  #scale_fill_brewer(palette="RdYlBu") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
+  labs(title= "Temperature Increase per Year 1900 - 1979",x="Longitude", y= "Latitude") 
+## cut out formatting fluff for debugging
+ggsave('pre_temp_cors.png')
+
+gg <- ggplot() + geom_polygon(data = to_plot, na.rm=TRUE, aes(x=long, y=lat, group = group,
+                                                              fill = as.numeric(to_plot[,'post_temp_cors'])),color=NA) +
+  guides(colour="none") + 
+  #geom_line(linetype="dotted", color="black", size=5) +
+  coord_fixed()+
+  scale_fill_gradient2(low = '#ffd9b3', mid="#ff8000", high = "#804000", midpoint = median(to_plot[,'post_temp_cors'], na.rm = TRUE), guide = guide_colorbar(title='Correlation Coefficients (ºC/year)')) +
+  #scale_color_gradientn(colours=rev(brewer.pal(4,"RdYlBu")),guide = guide_colorbar(title='value')) +
+  #scale_fill_brewer(palette="RdYlBu") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
+  labs(title= "Temperature Increase per Year 1980 - 2017",x="Longitude", y= "Latitude") 
+## cut out formatting fluff for debugging
+ggsave('post_temp_cors.png')
+
+# PPT CORRELATIONS
+gg <- ggplot() + geom_polygon(data = to_plot, na.rm=TRUE, aes(x=long, y=lat, group = group,
+                                                              fill = as.numeric(to_plot[,'full_ppt_cors'])),color=NA) +
+  guides(colour="none") + 
+  #geom_line(linetype="dotted", color="black", size=5) +
+  coord_fixed()+
+  scale_fill_gradient2(low = '#e6e6ff', mid="#0000ff", high = "#000080", midpoint = median(to_plot[,'full_ppt_cors'], na.rm = TRUE), guide = guide_colorbar(title='Correlation Coefficients (cm/year)')) +
+  #scale_color_gradientn(colours=rev(brewer.pal(4,"RdYlBu")),guide = guide_colorbar(title='value')) +
+  #scale_fill_brewer(palette="RdYlBu") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
+  labs(title= "Precipitation Increase per Year 1900 - 2017",x="Longitude", y= "Latitude") 
+## cut out formatting fluff for debugging
+ggsave('full_ppt_cors.png')
+
+gg <- ggplot() + geom_polygon(data = to_plot, na.rm=TRUE, aes(x=long, y=lat, group = group,
+                                                              fill = as.numeric(to_plot[,'post_ppt_cors'])),color=NA) +
+  guides(colour="none") + 
+  #geom_line(linetype="dotted", color="black", size=5) +
+  coord_fixed()+
+  scale_fill_gradient2(low = '#e6e6ff', mid="#0000ff", high = "#000080", midpoint = median(to_plot[,'post_ppt_cors'], na.rm = TRUE), guide = guide_colorbar(title='Correlation Coefficients (cm/year)')) +
+  #scale_color_gradientn(colours=rev(brewer.pal(4,"RdYlBu")),guide = guide_colorbar(title='value')) +
+  #scale_fill_brewer(palette="RdYlBu") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
+  labs(title= "Precipitation Increase per Year 1980 - 2017",x="Longitude", y= "Latitude") 
+## cut out formatting fluff for debugging
+ggsave('post_ppt_cors.png')
+
+gg <- ggplot() + geom_polygon(data = to_plot, na.rm=TRUE, aes(x=long, y=lat, group = group,
+                                                              fill = as.numeric(to_plot[,'pre_ppt_cors'])),color=NA) +
+  guides(colour="none") + 
+  #geom_line(linetype="dotted", color="black", size=5) +
+  coord_fixed()+
+  scale_fill_gradient2(low = '#e6e6ff', mid="#0000ff", high = "#000080", midpoint = median(to_plot[,'pre_ppt_cors'], na.rm = TRUE), guide = guide_colorbar(title='Correlation Coefficients (cm/year)')) +
+  #scale_color_gradientn(colours=rev(brewer.pal(4,"RdYlBu")),guide = guide_colorbar(title='value')) +
+  #scale_fill_brewer(palette="RdYlBu") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "grey92", colour = NA),title= element_text(size=10,hjust = 0.5,vjust = 1,face= c("bold"))) +
+  labs(title= "Precipitation Increase per Year 1900 - 1979",x="Longitude", y= "Latitude") 
+## cut out formatting fluff for debugging
+ggsave('pre_ppt_cors.png')
 
 #TEMPERATURE
 gg <- ggplot() + geom_polygon(data = to_plot, na.rm=TRUE, aes(x=long, y=lat, group = group,
@@ -156,7 +230,7 @@ gg <- ggplot() + geom_polygon(data = to_plot, na.rm=TRUE, aes(x=long, y=lat, gro
   guides(colour="none") + 
   #geom_line(linetype="dotted", color="black", size=5) +
   coord_fixed()+
-  scale_fill_gradient2(low = '#ffd9b3', mid="#ff8000", high = "#804000", midpoint = median(to_plot[,'temp'], na.rm = TRUE), guide = guide_colorbar(title='Degrees ºC')) +
+  scale_fill_gradient2(low = '#e6e6ff', mid="#0000ff", high = "#000080", midpoint = median(to_plot[,'temp'], na.rm = TRUE), guide = guide_colorbar(title='Degrees ºC')) +
   #scale_color_gradientn(colours=rev(brewer.pal(4,"RdYlBu")),guide = guide_colorbar(title='value')) +
   #scale_fill_brewer(palette="RdYlBu") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
