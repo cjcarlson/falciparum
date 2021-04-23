cbb6b4b3afd37e62bf4ecb85ecb560a72a985c87
Tamma Carleton <tammacarleton@EPIC-14653D.local>
2021-04-22 21:51:42 -0700

    updated fig 1 plotting script

--- fig1_responsefunctions.R
+++ fig1_responsefunctions.R
@@ -30,6 +30,7 @@ library(lfe)
 library(reshape)
 library(tidyverse)
 library(cowplot)
+library("tidyr")
 
 ########################################################################
 # A. Read in saved regression results
@@ -59,6 +60,10 @@ for(f in 1:length(files)){
   df = df %>% add_row(as.data.frame(tmp))
 }
 
+########################################################################
+# B. Histogram of optimal temperatures
+########################################################################
+
 # calculate optimal temperature
 df$optT = optT(df$temp,df$temp2)
 
@@ -75,21 +80,89 @@ g
 fileout = file.path("Results", "Figures", "Fig1","full_density_optimalT.pdf")
 ggsave(fileout)
 
-# limit to middle 99% of the data
-quants = quantile(df$optT, probs=c(.005,.995))
+# limit to middle 99.5% of the data
+quants = quantile(df$optT, probs=c(.01,.9925))
 dfsub = subset(df, optT>=quants[1] & optT<=quants[2])
 medoptT = median(dfsub$optT)
 g = ggplot(data=dfsub, aes(optT)) + 
   geom_density(col="grey", 
                fill="grey", 
                alpha = .2) + 
-  labs(title="Histogram of optimal temperatures", x="Optimal T (C)", y="count") + 
+  labs(title="Histogram of optimal temperatures", x="Optimal T (C)", y="density") + 
   geom_vline(xintercept=24, color="red", size=.5) + 
-  geom_vline(xintercept=medoptT, color="blue", size=.5) + theme_classic()
+  geom_vline(xintercept=medoptT, color="blue", size=.5) + theme_classic() +
+  annotate(geom="text", x=18, y=0.2, label="Theoretical benchmark",
+           color="red") +
+  annotate(geom="segment", x=22, xend=23.8, y=0.2, yend=0.2, arrow = arrow(length = unit(.2,"cm")),
+           color="red") +
+annotate(geom="text", x=17.5, y=0.22, label="Empirical median",
+         color="blue") +
+  annotate(geom="segment", x=20.6, xend=22.4, y=0.22, yend=0.22, arrow = arrow(length = unit(.2,"cm")),
+           color="blue") +
+  theme(text = element_text(size=18))
 g
-fileout = file.path("Results", "Figures", "Fig1","clipped_99_density_optimalT.pdf")
-ggsave(fileout)
+fileout = file.path("Results", "Figures", "Fig1","clipped_01_9925_density_optimalT.pdf")
+ggsave(fileout, plot=g)
 
+########################################################################
+# C. Spaghetti plot of estimated T response functions
+########################################################################
 
+#setup 
+Tref = 24
+Tmin = 10
+Tmax = 40
+int = 1
+plotXtemp = cbind(seq(Tmin,Tmax,by=int), seq(Tmin,Tmax,by=int)^2)
+xValsT = genRecenteredXVals_polynomial(plotXtemp,Tref,2)
+
+# point estimate
+main = subset(df, model=="main")
+b = as.matrix(c(main$temp, main$temp2))
+response = as.matrix(xValsT) %*% b #Prediction
+
+plotData = data.frame(x = xValsT[,1] + Tref, response = response)
+
+  g = ggplot(data = plotData) + geom_line(mapping = aes(x = x, y = response), color = "black", size=1.5) +
+    theme_classic() + geom_hline(yintercept = 0) + labs(x = "Daily average temperature (C)" , y = "Prevalence") +
+    theme(text = element_text(size=16))
+  g
+
+#plotData$model = "main"    
+
+# loop over all bootstraps, add to dataframe
+for(mod in 2:dim(df)[1]){ #dim(df)[1]
+  sub = df[mod,]
+  b = as.matrix(c(sub$temp,sub$temp2))
+  boot = as.data.frame(as.matrix(xValsT) %*% b) #Prediction
+  colnames(boot) = paste0("boot",mod)
+  plotData = cbind(plotData, boot)
+  
+  # progress
+  if(mod/100==round(mod/100)) {
+    print(paste0('--------- DONE WITH ITERATION ', mod, ' of 1000 --------'))
+  }
+}
+
+# reshape
+colnames(plotData)[2] = "boot1"
+plotData = plotData %>% gather(plotData, response, boot1:boot996)  
+colnames(plotData) = c("x", "model","response")  
+
+# plot
+g = ggplot()  +
+  geom_line(data=subset(plotData,model!="boot1"),aes(x = x, y = response, group = model), color = "coral3", alpha=.05) +
+  geom_line(data = subset(plotData,model=="boot1"), mapping = aes(x = x, y = response), color = "black", size=1.5) +
+  theme_cowplot() + geom_hline(yintercept = 0) + labs(x = "Daily average temperature (C)" , y = "Prevalence") 
+g
+
+fileout = file.path("Results", "Figures", "Fig1","Tresponse_bootstrapped.pdf")
+ggsave(fileout, plot=g)
+
+# TODO: ADD TEMP HISTOGRAM UNDERNEATH
+
+########################################################################
+# D. Lagged drought and flood responses
+########################################################################
 
   
