56ff1e7d49493edfed8d891ceb05859d7efd0286
Colin J. Carlson <cjcarlson@berkeley.edu>
2022-12-25 13:53:22 -0500

    Some final reorg of main pipeline

---
+++ Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R
@@ -0,0 +1,80 @@
+
+swirl <- function(input.raster) {
+  input.raster <- flip(t(input.raster),direction='y')
+  extent(input.raster) <- c(-180,180,-90,90)
+  input.raster = crop(input.raster, c(-30,60,-37,40))
+  return(input.raster)
+}
+
+
+r0t <- function(T, na.rm=TRUE) {
+  suppressWarnings(if(is.na(T)) return(NA))
+  if(T<=14) {return(0)}
+  a = 0.000203*T*(T-11.7)*((42.3-T)^0.5)
+  bc = -0.54*T*T + 25.2*T - 206
+  p = -0.000828*T*T + 0.0367*T + 0.522 # e^-mu
+  mu = -1*log(p)
+  PDR = 0.000111*T*(T-14.7)*((34.4-T)^0.5) # 1/EIP
+  pEA = -0.00924*T*T + 0.453*T - 4.77
+  MDR = 0.000111*T*(T-14.7)*((34-T)^0.5) # 1/tauEA
+  EFD = -0.153*T*T + 8.61*T - 97.7
+  
+  R0 = (((a^2)*bc*(p^(1/PDR))*EFD*pEA*MDR)/(mu^3))^(1/2)
+  if(is.nan(R0)){return(0)}
+  return(R0/87.13333) # that's the max
+}
+
+g <- ncvar_get(nct, 'tasN')
+
+#############
+
+cont <- readOGR(dsn = 'C:/Users/cjcar/Dropbox/MalariaAttribution/Data', layer =  'AfricaADM1')
+month = c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec')
+
+
+########################### R0
+
+for (i in 1:1368) { #1416
+  r <- swirl(raster(g[,,i]))
+  name=paste(paste(month[(i-1)%%12 + 1], ((i-1 - (i-1)%%12)/12)+1901,sep='.'),'temp',sep='.')
+  cont@data[,name] <- unlist(lapply(velox(r)$extract(cont,
+                                                     fun = function(x) mean(x, na.rm = TRUE),
+                                                     small=TRUE),
+                                    mean,na.rm=TRUE))
+  
+  c <- r*r
+  name=paste(paste(month[(i-1)%%12 + 1], ((i-1 - (i-1)%%12)/12)+1901,sep='.'),'temp2',sep='.')
+  cont@data[,name] <- unlist(lapply(velox(c)$extract(cont,
+                                                     fun = function(x) mean(x, na.rm = TRUE),
+                                                     small=TRUE),
+                                    mean,na.rm=TRUE))
+  print(i)
+}
+
+####################
+
+g <- ncvar_get(ncp, 'precN')
+
+for (i in 1:1368) { #1416
+  r <- swirl(raster(g[,,i]))
+  name=paste(paste(month[(i-1)%%12 + 1], ((i-1 - (i-1)%%12)/12)+1901,sep='.'),'ppt',sep='.')
+  cont@data[,name] <- unlist(lapply(velox(r)$extract(cont,
+                                                     fun = function(x) mean(x, na.rm = TRUE),
+                                                     small=TRUE),
+                                    mean,na.rm=TRUE))
+  print(i)
+}
+
+
+###########
+
+library(reshape)
+contdf <- cont@data
+contdf <- contdf[,-c(2:15)]
+contdf <- melt(contdf, id='OBJECTID')
+
+dffix <- separate(contdf,  variable, into=c('month','year','var'), sep='\\.')
+dffix <- cast(dffix, OBJECTID+month+year ~ var)
+dffix$year <- as.numeric(dffix$year)
+
+write.csv(dffix, paste("C:/Users/cjcar/Dropbox/MalariaAttribution/ClimateCSVs",paste(outname,'.csv',sep=''), sep='/'))
