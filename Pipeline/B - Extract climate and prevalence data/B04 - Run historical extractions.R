00652eff1fc09eb60558ec0f4eea19f7e30d82d5
Colin J. Carlson <cjcarlson@berkeley.edu>
2023-07-01 18:53:21 -0400

    Big scrub, fresh run, files fixed to run historical climates

--- Pipeline/B - Extract climate and prevalence data/B04 - Run historical extractions.R
+++ Pipeline/B - Extract climate and prevalence data/B04 - Run historical extractions.R
@@ -1,4 +1,6 @@
 
+
+
 library(ncdf4)
 library(raster)
 library(rgdal)
@@ -6,162 +8,149 @@ library(sp)
 library(tidyr)
 library(velox)
 
-setwd('G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data')
+setwd('G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/')
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_BCC-CSM2-MR_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")     
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_BCC-CSM2-MR_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_ACCESS-CM2_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")     
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_ACCESS-CM2_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")
+outname <- 'ACCESS-CM2'
+
+source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
+rm(list=ls())
+
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_ACCESS-ESM1-5_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")  
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_ACCESS-ESM1-5_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")   
+outname <- 'ACCESS-ESM1'
+
+source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
+rm(list=ls())
+
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_BCC-CSM2-MR_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")          
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_BCC-CSM2-MR_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")  
 outname <- 'BCC-CSM2-MR'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_CanESM5_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")  
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_CanESM5_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")   
-outname <- 'CanESM5'
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_CanESM5_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_CanESM5_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")    
+outname <- "CanESM5"
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_CESM2_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")          
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_CESM2_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")  
-outname <- 'CESM2'
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_FGOALS-g3_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")       
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_FGOALS-g3_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")  
+outname <- "FGOALS-g3"      
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_CNRM-CM6-1_historical_r1i1p1f2_gr_190101-201412_lonlat.nc")
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_CNRM-CM6-1_historical_r1i1p1f2_gr_190101-201412_lonlat.nc")    
-outname <- "CNRM-CM6"
-
-source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
-rm(list=ls())
-
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_GFDL-ESM4_historical_r1i1p1f1_gr1_190101-201412_lonlat.nc")       
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_GFDL-ESM4_historical_r1i1p1f1_gr1_190101-201412_lonlat.nc")  
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_GFDL-ESM4_historical_r1i1p1f1_gr1_190101-201412_BC_lonlat.nc")    
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_GFDL-ESM4_historical_r1i1p1f1_gr1_190101-201412_BC_lonlat.nc")    
 outname <- "GFDL-ESM4"      
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_GISS-E2-1-G_historical_r1i1p1f2_gn_190101-201412_lonlat.nc")    
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_GISS-E2-1-G_historical_r1i1p1f2_gn_190101-201412_lonlat.nc")    
-outname <- "GISS-E2"      
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_190101-201412_BC_lonlat.nc")  
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_190101-201412_BC_lonlat.nc")  
+outname <- 'IPSL-CM6A-LR'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_HadGEM3-GC31-LL_historical_r1i1p1f3_gn_190101-201412_lonlat.nc")  
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_HadGEM3-GC31-LL_historical_r1i1p1f3_gn_190101-201412_lonlat.nc")  
-outname <- 'HadGEM3'
-
-source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
-rm(list=ls())
-
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_190101-201412_lonlat.nc")  
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_IPSL-CM6A-LR_historical_r1i1p1f1_gr_190101-201412_lonlat.nc")   
-outname <- 'IPSL-CM6A'
-
-source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
-rm(list=ls())
-
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_MIROC6_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")       
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_MIROC6_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")           
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_MIROC6_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")  
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_MIROC6_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")   
 outname <- 'MIROC6'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_MRI-ESM2-0_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")    
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_MRI-ESM2-0_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")        
-outname <- 'MRI-ESM2'  
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_MRI-ESM2-0_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")       
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_MRI-ESM2-0_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")           
+outname <- 'MRI-ESM2-0'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist/BC_pr_Amon_NorESM2-LM_historical_r1i1p1f1_gn_190101-201412_lonlat.nc") 
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist/BC_tas_Amon_NorESM2-LM_historical_r1i1p1f1_gn_190101-201412_lonlat.nc")        
-outname <- 'NorESM2'  
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/pr_Amon_NorESM2-LM_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")    
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/historical/tas_Amon_NorESM2-LM_historical_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")        
+outname <- 'NorESM2-LM'  
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B02 - Extract GCMs - historical.R")
 rm(list=ls())
-
 
 ######################
 
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_BCC-CSM2-MR_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")    
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_BCC-CSM2-MR_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")
-outname <- 'BCC-CSM2-nat'
+setwd('G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/')
+
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_ACCESS-CM2_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")     
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_ACCESS-CM2_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")
+outname <- 'ACCESS-CM2-nat'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_CanESM5_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")  
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_CanESM5_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")   
-outname <- 'CanESM5-nat'
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_ACCESS-ESM1-5_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")  
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_ACCESS-ESM1-5_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")   
+outname <- 'ACCESS-ESM1-nat'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_CESM2_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")          
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_CESM2_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")  
-outname <- 'CESM2-nat'
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_BCC-CSM2-MR_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")          
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_BCC-CSM2-MR_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")  
+outname <- 'BCC-CSM2-MR-nat'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_CNRM-CM6-1_hist-nat_r1i1p1f2_gr_190101-201412_lonlat.nc")
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_CNRM-CM6-1_hist-nat_r1i1p1f2_gr_190101-201412_lonlat.nc")    
-outname <- "CNRM-CM6-nat"
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_CanESM5_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_CanESM5_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")    
+outname <- "CanESM5-nat"
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_GFDL-ESM4_hist-nat_r1i1p1f1_gr1_190101-201412_lonlat.nc")       
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_GFDL-ESM4_hist-nat_r1i1p1f1_gr1_190101-201412_lonlat.nc")  
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_FGOALS-g3_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")       
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_FGOALS-g3_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")  
+outname <- "FGOALS-g3-nat"      
+
+source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
+rm(list=ls())
+
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_GFDL-ESM4_hist-nat_r1i1p1f1_gr1_190101-201412_BC_lonlat.nc")    
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_GFDL-ESM4_hist-nat_r1i1p1f1_gr1_190101-201412_BC_lonlat.nc")    
 outname <- "GFDL-ESM4-nat"      
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_GISS-E2-1-G_hist-nat_r1i1p1f2_gn_190101-201412_lonlat.nc")    
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_GISS-E2-1-G_hist-nat_r1i1p1f2_gn_190101-201412_lonlat.nc")    
-outname <- "GISS-E2-nat"      
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_IPSL-CM6A-LR_hist-nat_r1i1p1f1_gr_190101-201412_BC_lonlat.nc")  
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_IPSL-CM6A-LR_hist-nat_r1i1p1f1_gr_190101-201412_BC_lonlat.nc")  
+outname <- 'IPSL-CM6A-LR-nat'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_HadGEM3-GC31-LL_hist-nat_r1i1p1f3_gn_190101-201412_lonlat.nc")  
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_HadGEM3-GC31-LL_hist-nat_r1i1p1f3_gn_190101-201412_lonlat.nc")  
-outname <- 'HadGEM3-nat'
-
-source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
-rm(list=ls())
-
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_IPSL-CM6A-LR_hist-nat_r1i1p1f1_gr_190101-201412_lonlat.nc")  
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_IPSL-CM6A-LR_hist-nat_r1i1p1f1_gr_190101-201412_lonlat.nc")   
-outname <- 'IPSL-CM6A-nat'
-
-source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
-rm(list=ls())
-
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_MIROC6_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")       
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_MIROC6_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")           
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_MIROC6_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")  
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_MIROC6_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")   
 outname <- 'MIROC6-nat'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_MRI-ESM2-0_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")    
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_MRI-ESM2-0_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")        
-outname <- 'MRI-ESM2-nat'  
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_MRI-ESM2-0_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")       
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_MRI-ESM2-0_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")           
+outname <- 'MRI-ESM2-0-nat'
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
 
-ncp <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/precipitation/hist-nat/BC_pr_Amon_NorESM2-LM_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc") 
-nct <- brick("G:/Shared drives/MALARIA_AFRICA/Bias-Corrected_data/temperature/hist-nat/BC_tas_Amon_NorESM2-LM_hist-nat_r1i1p1f1_gn_190101-201412_lonlat.nc")        
-outname <- 'NorESM2-nat'  
+ncp <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/pr_Amon_NorESM2-LM_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")    
+nct <- brick("G:/Shared drives/MALARIA_AFRICA/BC-ClimateData20230626/4-Outputs/hist-nat/tas_Amon_NorESM2-LM_hist-nat_r1i1p1f1_gn_190101-201412_BC_lonlat.nc")        
+outname <- 'NorESM2-LM-nat'  
 
 source("~/Github/falciparum/Pipeline/B - Extract climate and prevalence data/B03 - Extract GCMs - natural counterfactual.R")
 rm(list=ls())
