855af03ad258a0c846bd85c74455aeb2ef6efe8a
Tamma Carleton <tammacarleton@EPIC-14653D.local>
2025-06-19 12:24:03 -0700

    WIP on analysis of residuals

--- Pipeline/D - Model sensitivity analyses and checks/D04 - Uncertainty analysis.R
+++ Pipeline/D - Model sensitivity analyses and checks/D04 - Uncertainty analysis.R
@@ -26,15 +26,6 @@ source(here::here("Pipeline", "A - Utility functions", "A00 - Configuration.R"))
 source(here::here("Pipeline", "A - Utility functions", "A01 - Utility code for calculations.R"))
 source(here::here("Pipeline", "A - Utility functions", "A02 - Utility code for plotting.R"))
 
-# CRUversion = "4.03" # "4.06"
-if (CRUversion=="4.03") {
-  resdir = file.path(datadir, "Results")
-} else if (CRUversion=="4.06") {
-  resdir = file.path(datadir, "Results_CRU-TS4-06")
-} else {
-  print('CRU version not supported! Use 4.03 or 4.06.')
-}
-
 ############################################################
 # Plotting toggles
 # Choose reference temperature for response function, as well
@@ -52,28 +43,83 @@ Tmax = 40 # max T for x axis
 #### Call external script for data cleaning
 source(here::here("Pipeline", "A - Utility functions", "A03 - Prep data for estimation.R"))
 
-
+#### Store output here
+resdir = file.path(datadir, "Results")
+dir.create(file.path(resdir, "Figures", "Diagnostics"), showWarnings = FALSE)
+dir.create(file.path(resdir, "Tables", "Diagnostics"), showWarnings = FALSE)
+dir.create(file.path(resdir, "Figures", "Diagnostics", "Residuals"), showWarnings = FALSE)
+dir.create(file.path(resdir, "Tables", "Diagnostics", "Residuals"), showWarnings = FALSE)
 
 ########################################################################
-# R2: Correlated errors
+# Estimate main model, store residuals
 ########################################################################
 
-# Are residuals correlated within countries? 
-resCntry = felm(res ~ monthyr + I(country), data=complete)
+# Formula 
+cXt2intrXm = as.formula(
+  paste0(
+    "PfPR2 ~ temp + temp2 + ", floodvars, " + ", droughtvars, 
+    " + I(intervention) + country:monthyr + country:monthyr2 | OBJECTID + as.factor(smllrgn):month | 0 | OBJECTID"
+  )
+)
+
+# Estimation & residuals
+mainmod = felm(data = complete, formula = cXt2intrXm)
+complete <- complete |> mutate(res = c(residuals(mainmod)))
+
+########################################################################
+# Is there correlation across errors within a country?
+# (same month, year)
+########################################################################
+
+# Regress residuals on country dummies, control for month and year
+resCntry = felm(res ~ I(country) | month + year | 0 | 0 , data=complete)
+
+# Histogram of p-values on each country's coefficient
 pvals = summary(resCntry)$coefficients[,"Pr(>|t|)"]
 ph = ggplot() + 
   geom_histogram(aes(x=pvals), color= "seagreen", fill = "seagreen") + 
-  xlab("p-values by country") +
+  xlab("country p-value (null: no correlation within country)") + ylab("count of countries") +
   theme_classic()
 ph
+ggsave(file.path(resdir, "Figures", "Diagnostics", "Residuals", "pvals_country_correlations.pdf"), plot = ph, width = 7, height = 7)
 
-ggsave(file.path(resdir, "Figures", "Diagnostics", "Reviewer_comments", "res_corr_countries.pdf"), plot = ph, width = 7, height = 7)
-
-#boxplot of residuals by country
+# Boxplot of residuals by country 
 g= ggplot(complete, aes(x=country, y=res)) + 
   geom_boxplot() + 
   theme_classic() + ylab("residuals") + theme( axis.text.x=element_blank(),axis.ticks.x=element_blank())
 g
-ggsave(file.path(resdir, "Figures", "Diagnostics", "Reviewer_comments", "res_boxplot_countries.pdf"), plot = g, width = 9, height = 5)
+ggsave(file.path(resdir, "Figures", "Diagnostics", "Residuals", "residuals_country_boxplot.pdf"), plot = g, width = 9, height = 5)
+
+# Fstatistic
+df = data.frame(stat = c("F stat", "p value"), 
+                value = c(summary(resCntry)$P.fstat[5], summary(resCntry)$P.fstat[1]))
+write.csv(df, file.path(resdir, "Tables", "Diagnostics", "Residuals", "residuals_country_Fstat.csv"))
+
+########################################################################
+# Is there correlation across errors within a GBOD region
+# (same month, year)
+########################################################################
+
+# Regress residuals on country dummies, control for month and year
+resGBOD = felm(res ~ I(smllrgn) | month + year | 0 | 0 , data=complete)
+
+# Boxplot of residuals by country 
+g= ggplot(complete, aes(x=as.factor(smllrgn), y=res)) + 
+  geom_boxplot() + 
+  theme_classic() + ylab("residuals") + theme( axis.text.x=element_blank(),axis.ticks.x=element_blank())
+g
+ggsave(file.path(resdir, "Figures", "Diagnostics", "Residuals", "residuals_GBOD_boxplot.pdf"), plot = g, width = 9, height = 5)
+
+# Fstatistic
+df = data.frame(stat = c("F stat", "p value"), 
+                value = c(summary(resGBOD)$P.fstat[5], summary(resGBOD)$P.fstat[1]))
+write.csv(df, file.path(resdir, "Tables", "Diagnostics", "Residuals", "residuals_GBOD_Fstat.csv"))
+
+########################################################################
+# Is there correlation between close vs. far administrative units?
+########################################################################
+
+# TBD
+
 
 
