ab7ca1192650a4a63250734d6e6a309d879ad81b
cullen-molitor <cullen.molitor@gmail.com>
2025-07-09 13:14:45 -0700

    Add centroids

--- Pipeline/B - Extract climate and prevalence data/B01 - Extract CRU data and temperature for model.R
+++ Pipeline/B - Extract climate and prevalence data/B01 - Extract CRU data and temperature for model.R
@@ -2,9 +2,9 @@
 #### Use the following code to extract temperature and precipitation data from
 #### the CRU-TS4.XX data set. The code uses the exactextractr package to extract
 #### the data for each administrative unit. The code applies a power transformation
-#### to the data and calculates the mean for each administrative unit. It then 
+#### to the data and calculates the mean for each administrative unit. It then
 #### joins the data to the prevalence data and saves the final version in CSV format.
-#### -----------------------------------------------------------------------. 
+#### -----------------------------------------------------------------------.
 #### Written by: Cullen Molitor
 #### Date: 2024-10-08
 #### Email: cullen_molitor@ucsb.edu
@@ -27,43 +27,60 @@ source(here::here(pipeline_A_dir, "A01 - Utility code for calculations.R"))
 ### ADD THIS FUNCTION TO A01 - Utility code for calculations.R
 # Function to process each power
 process_clim_powers <- function(
-    power, 
-    clim_data, 
-    adm_data, 
-    rast_times,
-    var_name
+  power,
+  clim_data,
+  adm_data,
+  rast_times,
+  var_name
 ) {
-  
-  clim_data_k <- clim_data ^ power
-  
+  clim_data_k <- clim_data^power
+
   clim_extract_k <- exactextractr::exact_extract(
-    x = clim_data_k, y = adm_data, fun = 'mean', 
-    progress = TRUE, append_cols = c("OBJECTID")
+    x = clim_data_k,
+    y = adm_data,
+    fun = 'mean',
+    progress = TRUE,
+    append_cols = c("OBJECTID", "lon", "lat")
   )
-  
-  colnames(clim_extract_k) <- c("OBJECTID", rast_times)
-  
+
+  colnames(clim_extract_k) <- c("OBJECTID", "lon", "lat", rast_times)
+
   clim_extract_k <- clim_extract_k |>
     tidyr::pivot_longer(
-      cols = -OBJECTID, 
-      names_to = "date", 
+      cols = -c(OBJECTID, lon, lat),
+      names_to = "date",
       values_to = paste0(var_name, ifelse(power == 1, "", power))
-    ) |> 
-    tidyr::separate(date, into=c('year','month','day'), sep='-') |> 
+    ) |>
+    tidyr::separate(date, into = c('year', 'month', 'day'), sep = '-') |>
     dplyr::select(-day) |>
     dplyr::mutate(month = month.abb[as.numeric(month)])
-  
+
   return(clim_extract_k)
 }
 
 # Read shapefile
 cont <- sf::read_sf(here::here(datadir, 'Data', 'AfricaADM1.shp'))
 
+# Save centropids as lat/lon columns
+cont <- cont %>%
+  dplyr::mutate(
+    lon = sf::st_coordinates(sf::st_centroid(.))[, 1],
+    lat = sf::st_coordinates(sf::st_centroid(.))[, 2]
+  )
+
+ggplot() +
+  geom_sf(data = cont) +
+  geom_point(data = cont, aes(x = lon, y = lat), size = 0.5)
+
 # Read and process raster data
 tmp <- file.path(
-  datadir, "Data", "CRU_TS403_data", "tmp", 
-  "cru_ts4.03.1901.2018.tmp.dat.nc", 
-  "cru_ts4.03.1901.2018.tmp.dat.nc") |> 
+  datadir,
+  "Data",
+  "CRU_TS403_data",
+  "tmp",
+  "cru_ts4.03.1901.2018.tmp.dat.nc",
+  "cru_ts4.03.1901.2018.tmp.dat.nc"
+) |>
   terra::rast() |>
   terra::crop(cont) %>%
   terra::subset(grep("tmp_", names(.)))
@@ -75,28 +92,31 @@ powers <- 1:5
 
 # Apply the function to all powers and store in a list
 temp_extract_list <- lapply(
-  powers, 
-  process_clim_powers, 
-  clim_data = tmp, 
-  adm_data = cont, 
+  powers,
+  process_clim_powers,
+  clim_data = tmp,
+  adm_data = cont,
   rast_times = time_names,
   var_name = "temp"
 )
 
 # Merge all data frames into one
 temp_df <- purrr::reduce(
-  temp_extract_list, 
-  left_join, 
-  by = c("OBJECTID", 'year','month')
+  temp_extract_list,
+  left_join,
+  by = c("OBJECTID", 'year', 'month', 'lon', 'lat')
 )
-
 
 
 # Read and process raster data
 pre <- file.path(
-  datadir, "Data", "CRU_TS403_data", "pr", 
-  "cru_ts4.03.1901.2018.pre.dat.nc", 
-  "cru_ts4.03.1901.2018.pre.dat.nc") |> 
+  datadir,
+  "Data",
+  "CRU_TS403_data",
+  "pr",
+  "cru_ts4.03.1901.2018.pre.dat.nc",
+  "cru_ts4.03.1901.2018.pre.dat.nc"
+) |>
   terra::rast() |>
   terra::crop(cont) %>% # need magrittr for dot notation in names()
   terra::subset(grep("pre_", names(.)))
@@ -105,28 +125,28 @@ time_names <- as.character(time(pre))
 
 # Apply the function to all powers and store in a list
 pre_extract_list <- lapply(
-  powers, 
-  process_clim_powers, 
-  clim_data = pre, 
-  adm_data = cont, 
+  powers,
+  process_clim_powers,
+  clim_data = pre,
+  adm_data = cont,
   rast_times = time_names,
   var_name = "ppt"
 )
 
 # Merge all data frames into one
 pre_df <- purrr::reduce(
-  pre_extract_list, 
-  left_join, 
-  by = c("OBJECTID", 'year','month')
+  pre_extract_list,
+  left_join,
+  by = c("OBJECTID", 'year', 'month', 'lon', 'lat')
 )
 
 # Read the prevalence CSV file
 prev_df <- file.path(
-  datadir, 
-  "Data",  
-  'dataverse_files', 
+  datadir,
+  "Data",
+  'dataverse_files',
   '00 Africa 1900-2015 SSA PR database (260617).csv'
-) |> 
+) |>
   readr::read_csv(
     col_types = readr::cols(
       Long = col_double(),
@@ -136,7 +156,7 @@ prev_df <- file.path(
       Pf = col_double(),
       `PfPR2-10` = col_double()
     )
-  ) |> 
+  ) |>
   dplyr::mutate(
     METHOD = str_to_upper(METHOD)
   )
@@ -144,8 +164,10 @@ prev_df <- file.path(
 
 # Convert to sf object with POINT geometry
 prev_sf <- sf::st_as_sf(
-  prev_df, coords = c("Long", "Lat"), crs = 4326
-) 
+  prev_df,
+  coords = c("Long", "Lat"),
+  crs = 4326
+)
 
 # Join the prevalence data to the continent shapefile
 prev_with_cont <- sf::st_join(prev_sf, cont)
@@ -163,11 +185,10 @@ prev_with_cont <- sf::st_join(prev_sf, cont)
 #     Pf = mean(Pf, na.rm = TRUE),
 #     PfPR2 = mean(`PfPR2-10`, na.rm = TRUE),
 #     .groups = 'drop'
-#   ) |> 
+#   ) |>
 #   dplyr::mutate(
 #     year = as.character(year),
 #     month = as.character(month))
-
 
 # aggregated_data <- prev_with_cont %>%
 #   as_tibble() %>%
@@ -177,19 +198,18 @@ prev_with_cont <- sf::st_join(prev_sf, cont)
 #     year = YY
 #   ) %>%
 #   group_by(OBJECTID, year, month) %>%
-#   summarise( n_methods = n_distinct(METHOD)) |> 
+#   summarise( n_methods = n_distinct(METHOD)) |>
 #   dplyr::filter(n_methods > 1)
-
 
 # Calculate Mean Pf and PfPR2-10 per ADM1, Year, Month
 mean_data <- prev_with_cont %>%
   as_tibble() %>%
-  dplyr::select(OBJECTID, MM, YY, Pf, `PfPR2-10`, METHOD) %>%
+  dplyr::select(OBJECTID, MM, YY, Pf, `PfPR2-10`, METHOD, lon, lat) %>%
   dplyr::mutate(
     month = factor(MM, levels = 1:12, labels = month.abb),
     year = YY
   ) %>%
-  dplyr::group_by(OBJECTID, year, month) %>%
+  dplyr::group_by(OBJECTID, year, month, lon, lat) %>%
   dplyr::summarise(
     Pf_mean = mean(Pf, na.rm = TRUE),
     PfPR2_mean = mean(`PfPR2-10`, na.rm = TRUE),
@@ -200,37 +220,43 @@ mean_data <- prev_with_cont %>%
 # Add a simplified method to limit the number of categories
 dominant_method <- prev_with_cont %>%
   as_tibble() %>%
-  dplyr::select(OBJECTID, MM, YY, `PfPR2-10`, METHOD) %>%
+  dplyr::select(OBJECTID, MM, YY, `PfPR2-10`, METHOD, lon, lat) %>%
   dplyr::mutate(
     month = factor(MM, levels = 1:12, labels = month.abb),
     year = YY
   ) %>%
-  dplyr::group_by(OBJECTID, year, month, METHOD) %>%
+  dplyr::group_by(OBJECTID, year, month, METHOD, lon, lat) %>%
   dplyr::summarise(count = n(), .groups = 'drop') %>%
   dplyr::group_by(OBJECTID, year, month) %>%
   dplyr::slice_max(order_by = count, n = 1, with_ties = FALSE) %>%
   dplyr::ungroup() %>%
-  dplyr::select(OBJECTID, year, month, dominant_METHOD = METHOD) |> 
+  dplyr::select(OBJECTID, year, month, dominant_METHOD = METHOD, lon, lat) |>
   dplyr::mutate(
     simplified_METHOD = case_when(
-      dominant_METHOD %in% c("RDT", "RDT/SLIDE CONFIRMED", "RDT/PCR CONFIRMED") ~ "RDT",
-      dominant_METHOD %in% c("MICROSCOPY", "MICROSCOPY/PCR CONFIRMED") ~ "MICROSCOPY",
+      dominant_METHOD %in%
+        c("RDT", "RDT/SLIDE CONFIRMED", "RDT/PCR CONFIRMED") ~
+        "RDT",
+      dominant_METHOD %in% c("MICROSCOPY", "MICROSCOPY/PCR CONFIRMED") ~
+        "MICROSCOPY",
       TRUE ~ dominant_METHOD
     )
   )
 
 readr::write_csv(
-  dominant_method, 
+  dominant_method,
   file.path(
-    datadir, 
-    "Data", 
+    datadir,
+    "Data",
     paste0('dominant_diagnostic_method_summary.csv')
   )
 )
 
 # Merge Mean Data with Dominant Method
 aggregated_data <- mean_data %>%
-  left_join(dominant_method, by = c("OBJECTID", "year", "month")) %>%
+  left_join(
+    dominant_method,
+    by = c("OBJECTID", "year", "month", "lon", "lat")
+  ) %>%
   mutate(
     year = as.character(year),
     month = as.character(month)
@@ -238,21 +264,21 @@ aggregated_data <- mean_data %>%
 
 # Join the temperature, precipitation, and prevalence data
 complete_df <- dplyr::left_join(
-  temp_df, 
-  pre_df, 
-  by = c("OBJECTID", "year", "month")
-) |> 
+  temp_df,
+  pre_df,
+  by = c("OBJECTID", "year", "month", "lon", "lat")
+) |>
   dplyr::left_join(
-    aggregated_data, 
-    by = c("OBJECTID", "year", "month")
+    aggregated_data,
+    by = c("OBJECTID", "year", "month", "lon", "lat")
   )
 
 # Save the complete data frame
 readr::write_csv(
-  complete_df, 
+  complete_df,
   file.path(
-    datadir, 
-    "Data", 
+    datadir,
+    "Data",
     paste0('CRU-', CRUversion, '-Reextraction-Oct2024.csv')
   )
 )
