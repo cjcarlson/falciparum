e776e3d15a308305806826f5aeabb7330b5ff6cf
cullen-molitor <cullen.molitor@gmail.com>
2026-03-12 13:06:39 -0700

    Monthly cleanup

--- Pipeline/E - Estimate historical and future prevalence/E07 - Prevalence summaries monthly.R
+++ Pipeline/E - Estimate historical and future prevalence/E07 - Prevalence summaries monthly.R
@@ -27,298 +27,146 @@ tictoc::tic("Total execution time")
 
 source(here::here("Pipeline", "A - Utility functions", "A00 - Configuration.R"))
 
-yr_1901 <- 1901:1905
-yr_2014 <- 2010:2014
-yr_2015 <- 2015:2019
-yr_2050 <- 2048:2052
-yr_2100 <- 2096:2100
-
 num_cores <- 50
-# log_file <- here::here("TempFiles", "parallel_log.txt")
-cl <- parallel::makeCluster(num_cores, type = "PSOCK", outfile = "")
+log_file <- here::here("TempFiles", "parallel_log.txt")
+cl <- parallel::makeCluster(num_cores, type = "PSOCK", outfile = log_file)
+# cl <- parallel::makeCluster(num_cores, type = "PSOCK", outfile = "")
 doParallel::registerDoParallel(cl)
 
-for (mode in c("historical", "future")) {
-  # mode <- "historical"
-  print(paste0("Starting: ", mode))
+mode <- "historical"
+print(paste0("Starting: ", mode))
 
-  if (mode == "historical") {
-    temp_files <- "HistoricalTempFiles"
-    overall_fn <- "Fig2Hist-model-monthly.csv"
-    region_fn <- "Fig3Regionals-model-monthly.csv"
-    # map_fn <- "Fig3Big.feather"
-    overall_yr_filter <- c(1900:1930) # This is actually 1901 to 1930 in practice
-    region_yr_filter <- c(1900:1930) # This is actually 1901 to 1930 in practice
-  } else {
-    temp_files <- "FutureTempFiles"
-    overall_fn <- "Fig2Future-model-monthly.csv"
-    region_fn <- "Fig4Regionals-model-monthly.csv"
-    # map_fn <- "Fig4Big.feather"
-    overall_yr_filter <- c(2015:2019)
-    region_yr_filter <- c(2015:2020)
+region_fn <- "Fig3Regionals-monthly.csv"
+region_mod_fn <- "Fig3Regionals-model-monthly.csv"
+
+adm1_fn <- "Fig3ADM1-monthly.csv"
+
+iter_dir <- file.path(datadir, "IterationFiles", "HistoricalTempFiles")
+
+meta <- file.path(iter_dir, "RowMetadata.feather") |>
+  arrow::read_feather(
+    col_select = c(OBJECTID, year, scenario, model, country, region, month)
+  )
+
+####
+#### Data summaries ----
+####
+
+iter.list <- foreach(i = 1:1000) %dopar%
+  {
+    tictoc::tic(i)
+
+    cat(i, "Reading data\n")
+    iter <- file.path(iter_dir, paste0("iter_", i, ".feather")) |>
+      arrow::read_feather(col_select = "Pred") |>
+      data.table::as.data.table() |>
+      dplyr::bind_cols(meta) |>
+      dplyr::filter(year %in% 2010:2014)
+
+    cat(i, "Cleaning data\n")
+
+    scen_yr_reg_mean <- iter[,
+      list(Pred = mean(Pred, na.rm = TRUE)),
+      by = .(scenario, model, year, month, region)
+    ]
+
+    scen_yr_adm1_mean <- iter[,
+      list(Pred = mean(Pred, na.rm = TRUE)),
+      by = .(OBJECTID, scenario, model, year, month, region)
+    ]
+
+    scen_yr_reg_mean$run <- i
+    scen_yr_adm1_mean$run <- i
+
+    tictoc::toc()
+
+    return(list(scen_yr_reg_mean, scen_yr_adm1_mean)) # iter.list <- list(scen_yr_reg_mean)
   }
-  iter_dir <- file.path(datadir, "IterationFiles", temp_files)
 
-  meta <- file.path(iter_dir, "RowMetadata.feather") |>
-    arrow::read_feather(
-      col_select = c(OBJECTID, year, scenario, model, country, region, month)
-    )
-
-  if (mode == "historical") {
-    rows <- which(
-      (meta$scenario %in% scenarios[1:2]) & (meta$year %in% c(yr_1901, yr_2014))
-    )
-  } else {
-    rows <- which(
-      (meta$scenario %in% scenarios[3:5]) &
-        (meta$year %in% c(yr_2015, yr_2050, yr_2100))
-    )
-  }
-
-  ####
-  #### Data summaries ----
-  ####
-
-  iter.list <- foreach(i = 1:1000) %dopar%
-    {
-      tictoc::tic(i)
-
-      cat(i, "Reading data\n")
-      iter <- file.path(iter_dir, paste0("iter_", i, ".feather")) |>
-        arrow::read_feather(col_select = "Pred") |>
-        data.table::as.data.table() |>
-        dplyr::bind_cols(meta)
-
-      cat(i, "Cleaning data\n")
-      maps_iter <- iter[rows, ]
-      maps_iter$year[maps_iter$year %in% yr_1901] <- 1901
-      maps_iter$year[maps_iter$year %in% yr_2014] <- 2014
-      maps_iter$year[maps_iter$year %in% yr_2015] <- 2015
-      maps_iter$year[maps_iter$year %in% yr_2050] <- 2050
-      maps_iter$year[maps_iter$year %in% yr_2100] <- 2100
-
-      scen_yr_mean <- iter[,
-        list(Pred = mean(Pred, na.rm = TRUE)),
-        by = .(scenario, model, year, month)
-      ]
-      scen_yr_reg_mean <- iter[,
-        list(Pred = mean(Pred, na.rm = TRUE)),
-        by = .(scenario, model, year, month, region)
-      ]
-      scen_yr_adm_mean <- maps_iter[,
-        list(Pred = mean(Pred, na.rm = TRUE)),
-        by = .(scenario, model, year, month, OBJECTID)
-      ]
-
-      scen_yr_mean$run <- i
-      scen_yr_reg_mean$run <- i
-      scen_yr_adm_mean$run <- i
-
-      tictoc::toc()
-
-      return(list(scen_yr_mean, scen_yr_reg_mean, scen_yr_adm_mean))
-    }
-
-  ####
-  #### Overall medians ----
-  ####
-
-  cat("Overall medians\n")
-  scen_yr_mean <- bind_rows(lapply(iter.list, function(x) x[[1]])) |>
-    tibble::as_tibble()
-
-  scen_yr_mean |>
-    dplyr::filter(year %in% overall_yr_filter) |>
-    dplyr::group_by(model, run, scenario) |>
-    dplyr::summarize(BetaMean = mean(Pred, na.rm = TRUE)) |>
-    dplyr::right_join(scen_yr_mean) |>
-    dplyr::mutate(Pred = (Pred - BetaMean)) |>
-    dplyr::select(-BetaMean) -> df
-
-  df |>
-    dplyr::group_by(scenario, model, year, month) |>
-    dplyr::summarize(
-      median = median(Pred, na.rm = TRUE),
-      upper = quantile(Pred, 0.95, na.rm = TRUE),
-      lower = quantile(Pred, 0.05, na.rm = TRUE)
-    ) -> hist.to.graph
-
-  print(paste0("Saving: ", overall_fn))
-  readr::write_csv(hist.to.graph, here::here("TempFiles", overall_fn))
-
-  ####
-  #### Regional medians ----
-  ####
-
-  cat("Regional medians\n")
-  scen_yr_reg_mean <- bind_rows(lapply(iter.list, function(x) x[[2]])) |>
-    tibble::as_tibble()
-
-  scen_yr_reg_mean |>
-    dplyr::filter(year %in% region_yr_filter) |>
-    dplyr::group_by(scenario, model, region, run) |>
-    dplyr::summarize(BetaMean = mean(Pred, na.rm = TRUE)) |>
-    dplyr::right_join(scen_yr_reg_mean) |>
-    dplyr::mutate(Pred = (Pred - BetaMean)) |>
-    dplyr::select(-BetaMean) -> df
-
-  df |>
-    dplyr::group_by(scenario, model, region, year, month) |>
-    dplyr::summarize(
-      median = median(Pred, na.rm = TRUE),
-      upper = quantile(Pred, 0.95, na.rm = TRUE),
-      lower = quantile(Pred, 0.05, na.rm = TRUE)
-    ) -> data.to.graph
-
-  print(paste0("Saving: ", region_fn))
-  readr::write_csv(data.to.graph, here::here("TempFiles", region_fn))
-}
 parallel::stopCluster(cl)
+
+####
+#### Regional medians ----
+####
+
+cat("Regional medians\n")
+
+diff_df <- bind_rows(lapply(iter.list, function(x) x[[1]])) |>
+# diff_df <- bind_rows(iter.list) |>
+  tibble::as_tibble() |>
+  tidyr::pivot_wider(
+    id_cols = c(month, year, model, region, run),
+    names_from = scenario,
+    values_from = Pred
+  ) |>
+  dplyr::mutate(diff = historical - `hist-nat`) |>
+  mutate(
+    month_num = match(month, month.abb),
+    date = make_date(year = year, month = month_num, day = 1)
+  )
+
+rgn_mean_diff_df <- diff_df |>
+  dplyr::group_by(region, month_num) |>
+  dplyr::summarize(
+    mean = mean(diff, na.rm = TRUE),
+    median = median(diff, na.rm = TRUE),
+    upper = quantile(diff, 0.95, na.rm = TRUE),
+    lower = quantile(diff, 0.05, na.rm = TRUE)
+  )
+
+print(paste0("Saving: ", region_fn))
+readr::write_csv(
+  x = rgn_mean_diff_df,
+  file = here::here("TempFiles", region_fn)
+)
+
+rgn_mod_mean_diff_df <- diff_df |>
+  dplyr::group_by(region, model, month_num) |>
+  dplyr::summarize(
+    mean = mean(diff, na.rm = TRUE),
+    median = median(diff, na.rm = TRUE),
+    upper = quantile(diff, 0.95, na.rm = TRUE),
+    lower = quantile(diff, 0.05, na.rm = TRUE)
+  )
+
+print(paste0("Saving: ", region_mod_fn))
+readr::write_csv(
+  x = rgn_mod_mean_diff_df,
+  file = here::here("TempFiles", region_mod_fn)
+)
+
+####
+#### ADM1 medians ----
+####
+
+cat("ADM1 medians\n")
+
+diff_adm1_df <- bind_rows(lapply(iter.list, function(x) x[[2]])) |>
+  tibble::as_tibble() |>
+  tidyr::pivot_wider(
+    id_cols = c(OBJECTID, month, year, model, region, run),
+    names_from = scenario,
+    values_from = Pred
+  ) |>
+  dplyr::mutate(diff = historical - `hist-nat`) |>
+  mutate(
+    month_num = match(month, month.abb),
+    date = make_date(year = year, month = month_num, day = 1)
+  )
+
+adm1_mean_diff_df <- diff_adm1_df |>
+  dplyr::group_by(region, OBJECTID, month_num) |>
+  dplyr::summarize(
+    mean = mean(diff, na.rm = TRUE),
+    median = median(diff, na.rm = TRUE),
+    upper = quantile(diff, 0.95, na.rm = TRUE),
+    lower = quantile(diff, 0.05, na.rm = TRUE)
+  )
+
+print(paste0("Saving: ", adm1_fn))
+readr::write_csv(
+  x = adm1_mean_diff_df,
+  file = here::here("TempFiles", adm1_fn)
+)
+
 tictoc::toc()
-
-
-# yr_1901 <- 1901:1905
-# yr_2014 <- 2010:2014
-# yr_2015 <- 2015:2019
-# yr_2050 <- 2048:2052
-# yr_2100 <- 2096:2100
-
-# num_cores <- 25
-# log_file <- here::here("TempFiles", "parallel_log.txt")
-# cl <- parallel::makeCluster(num_cores, type = "FORK", outfile = log_file)
-# doParallel::registerDoParallel(cl)
-
-# for (mode in c("historical", "future")) {
-#   # mode <- "historical"
-#   print(paste0("Starting: ", mode))
-
-#   if (mode == "historical") {
-#     temp_files <- "HistoricalTempFiles"
-#     overall_fn <- "Fig2Hist-monthly.csv"
-#     region_fn <- "Fig3Regionals-monthly.csv"
-#     overall_yr_filter <- c(1900:1930) # This is actually 1901 to 1930 in practice
-#     region_yr_filter <- c(1900:1930) # This is actually 1901 to 1930 in practice
-#   } else {
-#     temp_files <- "FutureTempFiles"
-#     overall_fn <- "Fig2Future-monthly.csv"
-#     region_fn <- "Fig4Regionals-monthly.csv"
-#     overall_yr_filter <- c(2015:2019)
-#     region_yr_filter <- c(2015:2020)
-#   }
-#   iter_dir <- file.path(datadir, "IterationFiles", temp_files)
-
-#   meta <- file.path(iter_dir, "RowMetadata.feather") |>
-#     arrow::read_feather(
-#       col_select = c(OBJECTID, year, scenario, model, country, region, month)
-#     )
-
-#   if (mode == "historical") {
-#     rows <- which(
-#       (meta$scenario %in% scenarios[1:2]) & (meta$year %in% c(yr_1901, yr_2014))
-#     )
-#   } else {
-#     rows <- which(
-#       (meta$scenario %in% scenarios[3:5]) &
-#         (meta$year %in% c(yr_2015, yr_2050, yr_2100))
-#     )
-#   }
-
-#   ####
-#   #### Data summaries ----
-#   ####
-
-#   iter.list <- foreach(i = 1:1000) %dopar%
-#     {
-#       tictoc::tic(i)
-#       cat(i, "Reading data\n")
-
-#       iter <- file.path(iter_dir, paste0("iter_", i, ".feather")) |>
-#         arrow::read_feather(col_select = "Pred") |>
-#         data.table::as.data.table() |>
-#         dplyr::bind_cols(meta)
-
-#       cat(i, "Cleaning data\n")
-#       maps_iter <- iter[rows, ]
-#       maps_iter$year[maps_iter$year %in% yr_1901] <- 1901
-#       maps_iter$year[maps_iter$year %in% yr_2014] <- 2014
-#       maps_iter$year[maps_iter$year %in% yr_2015] <- 2015
-#       maps_iter$year[maps_iter$year %in% yr_2050] <- 2050
-#       maps_iter$year[maps_iter$year %in% yr_2100] <- 2100
-
-#       cat(i, "Summarizing data\n")
-#       scen_yr_mean <- iter[,
-#         list(Pred = mean(Pred, na.rm = TRUE)),
-#         by = .(scenario, model, year, month)
-#       ]
-#       scen_yr_reg_mean <- iter[,
-#         list(Pred = mean(Pred, na.rm = TRUE)),
-#         by = .(scenario, model, year, region)
-#       ]
-#       scen_yr_adm_mean <- maps_iter[,
-#         list(Pred = mean(Pred, na.rm = TRUE)),
-#         by = .(scenario, model, year, OBJECTID)
-#       ]
-
-#       scen_yr_mean$run <- i
-#       scen_yr_reg_mean$run <- i
-#       scen_yr_adm_mean$run <- i
-
-#       tictoc::toc()
-
-#       return(list(scen_yr_mean, scen_yr_reg_mean, scen_yr_adm_mean))
-#     }
-
-#   ####
-#   #### Overall medians ----
-#   ####
-
-#   scen_yr_mean <- bind_rows(lapply(iter.list, function(x) x[[1]])) |>
-#     tibble::as_tibble()
-
-#   scen_yr_mean |>
-#     dplyr::filter(year %in% overall_yr_filter) |>
-#     dplyr::group_by(run, scenario) |>
-#     dplyr::summarize(BetaMean = mean(Pred, na.rm = TRUE)) |>
-#     dplyr::right_join(scen_yr_mean) |>
-#     dplyr::mutate(Pred = (Pred - BetaMean)) |>
-#     dplyr::select(-BetaMean) -> df
-
-#   df |>
-#     dplyr::group_by(scenario, year, month) |>
-#     dplyr::summarize(
-#       median = median(Pred, na.rm = TRUE),
-#       upper = quantile(Pred, 0.95, na.rm = TRUE),
-#       lower = quantile(Pred, 0.05, na.rm = TRUE)
-#     ) -> hist.to.graph
-
-#   print(paste0("Saving: ", overall_fn))
-#   readr::write_csv(hist.to.graph, here::here("TempFiles", overall_fn))
-
-#   ####
-#   #### Regional medians ----
-#   ####
-
-#   scen_yr_reg_mean <- bind_rows(lapply(iter.list, function(x) x[[2]])) |>
-#     tibble::as_tibble()
-
-#   scen_yr_reg_mean |>
-#     dplyr::filter(year %in% region_yr_filter) |>
-#     dplyr::group_by(scenario, region, run) |>
-#     dplyr::summarize(BetaMean = mean(Pred, na.rm = TRUE)) |>
-#     dplyr::right_join(scen_yr_reg_mean) |>
-#     dplyr::mutate(Pred = (Pred - BetaMean)) |>
-#     dplyr::select(-BetaMean) -> df
-
-#   df |>
-#     dplyr::group_by(scenario, region, year) |>
-#     dplyr::summarize(
-#       median = median(Pred, na.rm = TRUE),
-#       upper = quantile(Pred, 0.95, na.rm = TRUE),
-#       lower = quantile(Pred, 0.05, na.rm = TRUE)
-#     ) -> data.to.graph
-
-#   print(paste0("Saving: ", region_fn))
-#   readr::write_csv(data.to.graph, here::here("TempFiles", region_fn))
-# }
-# parallel::stopCluster(cl)
-# tictoc::toc()
--- Pipeline/G - Figure generation for supplement/G06 - Monthly time series.R
+++ Pipeline/G - Figure generation for supplement/G06 - Monthly time series.R
@@ -4,102 +4,282 @@ library(tidyverse)
 library(lubridate)
 library(patchwork)
 library(cowplot)
+library(knitr)
+library(kableExtra)
+library(broom)
 
-source(here::here("Pipeline", "A - Utility functions", "A00 - Configuration.R"))
-source(here::here(
-  "Pipeline",
-  "A - Utility functions",
-  "A01 - Utility code for calculations.R"
-))
-source(here::here(
-  "Pipeline",
-  "A - Utility functions",
-  "A02 - Utility code for plotting.R"
-))
+source(
+  here::here(
+    "Pipeline",
+    "A - Utility functions",
+    "A00 - Configuration.R"
+  )
+)
+source(
+  here::here(
+    "Pipeline",
+    "A - Utility functions",
+    "A01 - Utility code for calculations.R"
+  )
+)
+source(
+  here::here(
+    "Pipeline",
+    "A - Utility functions",
+    "A02 - Utility code for plotting.R"
+  )
+)
 
-hist.to.graph <- here::here("TempFiles", "Fig2Hist-monthly.csv") |>
-  vroom::vroom(show_col_types = FALSE)
+###### seasonal difference
 
-hist.to.graph |>
-  filter(scenario == 'historical', year %in% c(2010:2014)) |>
-  pull(median) |>
-  mean() -> base
-
-graph.data <- hist.to.graph |>
-  mutate(
-    scenario = factor(
-      scenario,
-      levels = c('hist-nat', 'historical', 'ssp126', 'ssp245', 'ssp585')
-    )
-  ) |>
-  filter(!(year %in% c(1901, 2015))) |>
-  mutate(
-    month_num = match(month, month.abb),
-    date = make_date(year = year, month = month_num, day = 1),
-    lower = pmax(lower, -1.75),
+rgn_mod_mon_df <- here::here("TempFiles", "Fig3Regionals-model-monthly.csv") |>
+  vroom::vroom(show_col_types = FALSE) |>
+  dplyr::mutate(
+    region = stringr::str_remove_all(region, "Sub-Saharan Africa \\("),
+    region = stringr::str_replace_all(region, "\\)", " Africa")
   )
 
-ggplot(
-  data = graph.data,
-  aes(x = date, y = median, group = scenario, color = scenario)
-) +
-  theme_bw() +
-  geom_hline(yintercept = 0, color = 'grey30', lwd = 0.2) +
-  scale_color_manual(
-    values = c("grey50", "#287DAB", "#4d5f8e", "#C582B2", "#325756"),
-    labels = scenario_labels,
-    name = ''
-  ) +
-  scale_fill_manual(
-    values = c("grey50", "#287DAB", "#4d5f8e", "#C582B2", "#325756"),
-    labels = scenario_labels,
-    name = ''
-  ) +
-  # geom_vline(xintercept = 2014.5, linetype = 'dashed') +
-  geom_line(aes(x = date, y = median), lwd = 1) +
-  # geom_ribbon(
-  #   aes(ymin = lower, ymax = upper, colour = scenario),
-  #   fill = NA,
-  #   linewidth = 0.1,
-  #   show.legend = FALSE,
-  # ) +
-  # geom_ribbon(
-  #   aes(ymin = lower, ymax = upper, fill = scenario),
-  #   color = NA,
-  #   alpha = 0.1
-  # ) +
-  scale_x_date(
-    breaks = c(
-      as.Date("1900-01-01"),
-      as.Date("1925-01-01"),
-      as.Date("1950-01-01"),
-      as.Date("2000-01-01")
-    ),
-    # date_breaks = "25 years",
-    date_labels = "%Y",
-    expand = expansion(0.02, 0.1),
-  ) +
-  labs(x = 'Year', y = 'Prevalence (%)') +
-  theme(
-    axis.title.x = element_text(vjust = -3),
-    axis.title.y = element_text(vjust = 6),
-    plot.margin = unit(c(0.5, 0.5, 1, 1), "cm"),
-    legend.position = "inside",
-    legend.position.inside = c(0.1, 0.05),
-    legend.margin = margin(0, 0, 0, 0),
-    legend.text = element_text(size = rel(0.8)),
-    legend.title = element_blank()
-  ) -> s
-s
+rgn_mon_df <- here::here("TempFiles", "Fig3Regionals-monthly.csv") |>
+  vroom::vroom(show_col_types = FALSE) |>
+  dplyr::mutate(
+    region = stringr::str_remove_all(region, "Sub-Saharan Africa \\("),
+    region = stringr::str_replace_all(region, "\\)", " Africa")
+  )
 
-ggsave(
-  filename = "FigureS-mn-time-series.jpg",
-  plot = last_plot(),
+monthly_diff <- ggplot() +
+  geom_hline(data = NULL, yintercept = 0, colour = "black", linewidth = 0.3) +
+  geom_line(
+    data = rgn_mod_mon_df,
+    aes(
+      x = month_num,
+      y = median,
+      # color = region,
+      group = model
+    ),
+    alpha = 0.3,
+    linewidth = 1.25,
+    color = "grey60"
+  ) +
+  geom_line(
+    data = rgn_mon_df,
+    aes(
+      x = month_num,
+      y = median,
+      group = region
+    ),
+    linewidth = 1.5, # mean trace
+    colour = "#287DAB"
+  ) +
+  facet_wrap(~region, ncol = 1) +
+  scale_x_continuous(
+    breaks = 1:12,
+    labels = month.abb,
+    expand = c(0.02, 0) # small left/right padding
+  ) +
+  scale_y_continuous(
+    limits = c(-10, 10)
+  ) +
+  labs(
+    x = NULL,
+    y = "Change in prevalence (%)",
+    # title = "Avg Monthly difference (2010-2014) between ‘historical’ and ‘hist-nat’ scenarios",
+  ) +
+  theme_classic() +
+  theme(
+    plot.title = element_text(face = "bold"),
+    axis.title.y = element_text(vjust = 3),
+    axis.title.x = element_text(vjust = -2),
+    plot.margin = unit(c(0.5, 0.5, 1, 1), "cm"),
+    legend.position = "none",
+    # legend.position.inside = c(0.13, 0.85),
+  )
+
+monthly_diff
+
+ggplot2::ggsave(
+  filename = "FigureS-monthly-diff.pdf",
+  plot = monthly_diff,
   path = here::here("Figures"),
-  width = 12,
-  height = 6,
-  units = "in"
+  width = 7.42,
+  height = 10.07,
+  units = "in",
+  device = cairo_pdf,
+  dpi = 1200
 )
+
+
+regional_avg_effect <- here::here("TempFiles", "H04_results_summary.csv") |>
+  vroom::vroom(show_col_types = FALSE) |>
+  dplyr::filter(Region != "Sub-Saharan Africa (continent-wide)") |>
+  dplyr::rename(`Average Annual Impact (% points)` = MeanDifference)
+
+
+region_peak_effect <- rgn_mon_df |>
+  dplyr::group_by(region) |>
+  dplyr::slice_max(abs(median), n = 1, with_ties = FALSE) |>
+  dplyr::ungroup() |>
+  dplyr::mutate(
+    Region = region,
+    `Month of Peak Impact` = factor(month.abb[month_num], levels = month.abb),
+    `Impact Size (% points)` = median,
+    mon_Quantile_025 = lower,
+    mon_Quantile_975 = upper,
+    .keep = "none"
+  ) |>
+  dplyr::left_join(regional_avg_effect) |>
+  dplyr::arrange(Region) |>
+  dplyr::select(
+    Region,
+    `Month of Peak Impact`,
+    `Impact Size (% points)`,
+    mon_Quantile_025,
+    mon_Quantile_975,
+    `Average Annual Impact (% points)`,
+    Quantile_025,
+    Quantile_975
+  )
+
+
+region_peak_effect <- rgn_mon_df |>
+  dplyr::group_by(region) |>
+  dplyr::mutate(
+    # find the month with the max and min median values
+    max_month_val = max(median, na.rm = TRUE),
+    min_month_val = min(median, na.rm = TRUE),
+    # compute the range
+    monthly_range = max_month_val - min_month_val
+  ) |>
+  dplyr::slice_max(abs(median), n = 1, with_ties = FALSE) |>
+  dplyr::ungroup() |>
+  dplyr::mutate(
+    Region = region,
+    `Month of Peak Impact` = factor(month.abb[month_num], levels = month.abb),
+    `Impact Size (% points)` = median,
+    mon_Quantile_025 = lower,
+    mon_Quantile_975 = upper,
+    `Monthly Range (% points)` = monthly_range, # new column here
+    .keep = "none"
+  ) |>
+  dplyr::left_join(regional_avg_effect) |>
+  dplyr::arrange(Region) |>
+  dplyr::select(
+    Region,
+    `Month of Peak Impact`,
+    `Impact Size (% points)`,
+    mon_Quantile_025,
+    mon_Quantile_975,
+    `Monthly Range (% points)`,
+    `Average Annual Impact (% points)`,
+    Quantile_025,
+    Quantile_975
+  )
+
+# # LaTeX table (booktabs + makecell-style headers)
+# kableExtra::kable(
+#   region_peak_effect,
+#   format = "latex",
+#   digits = 4,
+#   booktabs = TRUE,
+#   align = c("l", "c", "c", "c"),
+#   col.names = c(
+#     "Region",
+#     "\\makecell{Month of \\\\ Peak Impact}",
+#     "\\makecell{Impact Size \\\\ (\\% points)}",
+#     "\\makecell{Average Annual Impact \\\\ (\\% points)}"
+#   ),
+#   escape = FALSE
+# ) |>
+#   kableExtra::kable_styling(latex_options = c("hold_position"))
+
+####################################################################
+####################################################################
+###### time series plot - full monthly time series - VERY UGLY
+####################################################################
+####################################################################
+
+# hist.to.graph <- here::here("TempFiles", "Fig2Hist-monthly.csv") |>
+#   vroom::vroom(show_col_types = FALSE)
+
+# hist.to.graph |>
+#   filter(scenario == 'historical', year %in% c(2010:2014)) |>
+#   pull(median) |>
+#   mean() -> base
+
+# graph.data <- hist.to.graph |>
+#   mutate(
+#     scenario = factor(
+#       scenario,
+#       levels = c('hist-nat', 'historical', 'ssp126', 'ssp245', 'ssp585')
+#     )
+#   ) |>
+#   filter(!(year %in% c(1901, 2015))) |>
+#   mutate(
+#     month_num = match(month, month.abb),
+#     date = make_date(year = year, month = month_num, day = 1),
+#     lower = pmax(lower, -1.75),
+#   )
+
+# ggplot(
+#   data = graph.data,
+#   aes(x = date, y = median, group = scenario, color = scenario)
+# ) +
+#   theme_bw() +
+#   geom_hline(yintercept = 0, color = 'grey30', lwd = 0.2) +
+#   scale_color_manual(
+#     values = c("grey50", "#287DAB", "#4d5f8e", "#C582B2", "#325756"),
+#     labels = scenario_labels,
+#     name = ''
+#   ) +
+#   scale_fill_manual(
+#     values = c("grey50", "#287DAB", "#4d5f8e", "#C582B2", "#325756"),
+#     labels = scenario_labels,
+#     name = ''
+#   ) +
+#   # geom_vline(xintercept = 2014.5, linetype = 'dashed') +
+#   geom_line(aes(x = date, y = median), lwd = 1) +
+#   # geom_ribbon(
+#   #   aes(ymin = lower, ymax = upper, colour = scenario),
+#   #   fill = NA,
+#   #   linewidth = 0.1,
+#   #   show.legend = FALSE,
+#   # ) +
+#   # geom_ribbon(
+#   #   aes(ymin = lower, ymax = upper, fill = scenario),
+#   #   color = NA,
+#   #   alpha = 0.1
+#   # ) +
+#   scale_x_date(
+#     breaks = c(
+#       as.Date("1900-01-01"),
+#       as.Date("1925-01-01"),
+#       as.Date("1950-01-01"),
+#       as.Date("2000-01-01")
+#     ),
+#     # date_breaks = "25 years",
+#     date_labels = "%Y",
+#     expand = expansion(0.02, 0.1),
+#   ) +
+#   labs(x = 'Year', y = 'Prevalence (%)') +
+#   theme(
+#     axis.title.x = element_text(vjust = -3),
+#     axis.title.y = element_text(vjust = 6),
+#     plot.margin = unit(c(0.5, 0.5, 1, 1), "cm"),
+#     legend.position = "inside",
+#     legend.position.inside = c(0.1, 0.05),
+#     legend.margin = margin(0, 0, 0, 0),
+#     legend.text = element_text(size = rel(0.8)),
+#     legend.title = element_blank()
+#   ) -> s
+# s
+
+# ggsave(
+#   filename = "FigureS-mn-time-series.jpg",
+#   plot = last_plot(),
+#   path = here::here("Figures"),
+#   width = 12,
+#   height = 6,
+#   units = "in"
+# )
 
 ###### seasonal sub-series - Last 5 years of data, averaged each month
 ###### to show prevelence pattern at the monthly level across a year
@@ -137,111 +317,3 @@ ggsave(
 #   ) +
 #   labs(x = NULL, y = "Prevalence (%)", title = "Seasonal sub-series") +
 #   theme_bw()
-
-###### seasonal difference
-
-hist.to.graph <- here::here("TempFiles", "Fig3Regionals-model-monthly.csv") |>
-  vroom::vroom(show_col_types = FALSE) |>
-  dplyr::mutate(
-    region = stringr::str_remove_all(region, "Sub-Saharan Africa \\("),
-    region = stringr::str_replace_all(region, "\\)", " Africa")
-  )
-
-graph.data <- hist.to.graph |>
-  mutate(
-    scenario = factor(
-      scenario,
-      levels = c('hist-nat', 'historical', 'ssp126', 'ssp245', 'ssp585')
-    )
-  ) |>
-  ### Start plotting in 1902 and 2016 because it's the first full year with lags incorporated right.
-  filter(!(year %in% c(1901, 2015))) |>
-  mutate(
-    month_num = match(month, month.abb),
-    date = make_date(year = year, month = month_num, day = 1),
-    lower = pmax(lower, -1.75),
-  )
-
-yr_order <- graph.data |>
-  distinct(year) |>
-  arrange(year) |>
-  pull(year)
-
-last5 <- tail(yr_order, 5)
-
-diff.data <- graph.data |>
-  filter(year %in% last5) |>
-  mutate(month_num = match(month, month.abb)) |>
-  pivot_wider(
-    id_cols = c(model, region, year, month, month_num),
-    names_from = scenario,
-    values_from = median
-  ) |>
-  ## calculate the difference:  historical − hist-nat
-  mutate(diff = historical - `hist-nat`)
-
-
-test <- diff.data |>
-  dplyr::group_by(region, model, month, month_num) |>
-  dplyr::summarise(
-    diff = mean(diff, na.rm = TRUE)
-  ) |>
-  dplyr::ungroup()
-
-test2 <- test |>
-  dplyr::group_by(region, month_num) |>
-  dplyr::summarise(
-    mean_month = mean(diff, na.rm = TRUE)
-  )
-
-monthly_diff <- ggplot() +
-  geom_hline(data = NULL, yintercept = 0, colour = "black", linewidth = 0.3) +
-  geom_line(
-    data = test,
-    aes(
-      x = month_num,
-      y = diff,
-      # color = region,
-      group = model
-    ),
-    alpha = 0.3,
-    linewidth = 1.25,
-    color = "grey60"
-  ) +
-  geom_line(
-    data = test2,
-    aes(x = month_num, y = mean_month),
-    linewidth = 1.5, # mean trace
-    colour = "#287DAB"
-  ) +
-  facet_wrap(~region, ncol = 1) +
-  scale_x_continuous(
-    breaks = 1:12,
-    labels = month.abb,
-    expand = c(0.02, 0) # small left/right padding
-  ) +
-  labs(
-    x = NULL,
-    y = "Change in prevalence (%)",
-    # title = "Avg Monthly difference (2010-2014) between ‘historical’ and ‘hist-nat’ scenarios",
-  ) +
-  theme_classic() +
-  theme(
-    plot.title = element_text(face = "bold"),
-    axis.title.y = element_text(vjust = 3),
-    axis.title.x = element_text(vjust = -2),
-    plot.margin = unit(c(0.5, 0.5, 1, 1), "cm"),
-    legend.position = "none",
-    # legend.position.inside = c(0.13, 0.85),
-  )
-
-ggplot2::ggsave(
-  filename = "FigureS-monthly-diff.pdf",
-  plot = monthly_diff,
-  path = here::here("Figures"),
-  width = 7.42,
-  height = 10.07,
-  units = "in",
-  device = cairo_pdf,
-  dpi = 1200
-)
